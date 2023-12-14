4dbf2d5d26d66d53c7fa91c2c4f046aad05b86a7
prakasarul <prakasarul222@gmail.com>
2023-12-14 19:24:35 +0000

    "updated instance"

---
+++ D1/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "ap-south-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "terra_insta" {
+    ami           = "ami-02a2af70a66af6dfb"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
