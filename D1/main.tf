d604489556b42615dcdeac70c23426b6f63e621d
prakasarul <prakasarul222@gmail.com>
2023-12-14 19:31:07 +0000

    update ec2code

--- D1/main.tf
+++ D1/main.tf
@@ -5,4 +5,5 @@ provider "aws" {
 resource "aws_instance" "terra_insta" {
     ami           = "ami-02a2af70a66af6dfb"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    key_name = ""
 }
